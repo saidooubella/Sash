dd2b246e78f466cfa8bc59fc8f4da85486d4f10c
Said Oubella <saidooubella@gmail.com>
2025-02-15 01:22:04 +0100

    feat: setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "saidooubella"
+version = "0.0.1-dev"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.6.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+    explicitApi()
+    compilerOptions {
+        freeCompilerArgs.add("-opt-in=kotlin.contracts.ExperimentalContracts")
+        freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
+        extraWarnings.set(true)
+    }
+}
